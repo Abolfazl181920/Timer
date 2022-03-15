6de5d2f536116b0680822353a72e53ae31587366
Abolfazl <abolfazlm181920@gmail.com>
2022-03-15 19:55:24 +0330

    renew seconds state in useEffect

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -6,12 +6,14 @@ const Pomodoro = () => {
     const [ seconds, setSeconds ] = useState(0);
     const [ displayMessage, setDisplayMessage ] = useState(false);
 
+    useEffect(() => {}, [ seconds ]);
+    
     return (
         <div className="pomodoro">
             <div className="message">
                 { displayMessage && <div>Break time! New sessions is coming...</div> }
             </div>
-            <div className="timer"> { minutes } : { seconds } </div>
+            <div className="timer"> { minutes }:{ seconds } </div>
         </div>
     );
 }
