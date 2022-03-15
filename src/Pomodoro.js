4a29b1f4ee2042c6968cc973493d7b2c48b28db0
Abolfazl <abolfazlm181920@gmail.com>
2022-03-15 19:40:39 +0330

    Pomodoro.js codes

---
+++ src/Pomodoro.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Pomodoro = () => {
+    return (
+        <div className="pomodoro">
+            <div className="message">
+                <div>Break time! New sessions is coming...</div>
+            </div>
+            <div className="timer">25:00</div>
+        </div>
+    );
+}
+
+export default Pomodoro;
