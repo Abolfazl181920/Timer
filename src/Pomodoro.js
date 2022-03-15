caafd48c3b248d2e1ff5f01a300761701d954f74
Abolfazl <abolfazlm181920@gmail.com>
2022-03-15 20:09:45 +0330

    add logics

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -5,15 +5,30 @@ const Pomodoro = () => {
     const [ minutes, setMinutes ] = useState(25);
     const [ seconds, setSeconds ] = useState(0);
     const [ displayMessage, setDisplayMessage ] = useState(false);
+    const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
 
-    useEffect(() => {}, [ seconds ]);
-    
+    useEffect(() => {
+        let interval = setInterval(() => {
+            clearInterval(interval);
+
+            if (seconds === 0) {
+                if (minutes !== 0) {
+                    setSeconds(59);
+                    setMinutes(minutes - 1);
+                }
+            } else {
+                setSeconds(seconds - 1);
+            }
+        }, 1000);
+    }, [ seconds ]);
+
     return (
         <div className="pomodoro">
             <div className="message">
                 { displayMessage && <div>Break time! New sessions is coming...</div> }
             </div>
-            <div className="timer"> { minutes }:{ seconds } </div>
+            <div className="timer"> { timerMinutes }:{ timerSeconds } </div>
         </div>
     );
 }
